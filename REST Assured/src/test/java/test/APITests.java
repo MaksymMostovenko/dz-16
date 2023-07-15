59d998f546668ac94e306625b2e083228f188b5b
MaksymMostovenko <blockmaksim@gmail.com>
2023-07-15 11:46:20 +0300

    wip домашка в роботі

--- REST Assured/src/test/java/test/APITests.java
+++ REST Assured/src/test/java/test/APITests.java
@@ -1,54 +1,35 @@
 package test;
 
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
 public class APITests {
-
-    public static void main(String[] args) {
-        String baseUrl = "https://restful-booker.herokuapp.com";
-        String endpoint = "/booking/{bookingId}";
-
-        Response response = RestAssured.given()
-                .pathParam("bookingId", 3191)
-                .get(baseUrl + endpoint);
-
-        int statusCode = response.getStatusCode();
-        String responseBody = response.getBody().asString();
-
-        System.out.println("Status Code: " + statusCode);
-        System.out.println("Response Body: " + responseBody);
-    }
-
     private TestSteps apiTestSteps;
-    @BeforeMethod
+    @BeforeTest
     public void setup() {
         apiTestSteps = new TestSteps();
-    }
-
-    @Test
-    public void authTest(){
-        apiTestSteps.authenticate()
-                .getAuthTocken()
-                .getCookie()
-                .assessStatusCode();
+        apiTestSteps.authenticate();
     }
 
     @Test
     public void addBookingTest(){
-        apiTestSteps.sendBookingRequest()
-                .getBookingDetails()
-                .assessStatusCode()
-                .assessBooking();
+        apiTestSteps.addNewBooking();
+        apiTestSteps.getBooking();
     }
 
     @Test
-    public void editBookingTest(){
-        apiTestSteps.editBooking(2286)
-                .assessStatusCode()
-                .assessEditBookingData();
+    public void getAllBookingTest(){
+        apiTestSteps.getAllBookingIds();
+    }
+
+    @Test
+    public void partialUpdateBookingTest(){
+        apiTestSteps.patchBookingPrice();
+    }
+
+    @Test
+    public void cangeNameNeedsTest(){
+
     }
 }
